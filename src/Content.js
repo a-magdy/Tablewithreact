10fe7c5b0378417616943acc0a7d4a39e7861b7e
Ahmad Magdy <magdy.a@hotmail.com>
2018-06-01 16:18:33 +0200

    Wrap ReactTable with a tenary condition to only render when this.props.data is provided

--- src/Content.js
+++ src/Content.js
@@ -1,59 +1,58 @@
-import React, { Component } from 'react';
-import ReactTable from 'react-table'
+import React, { Component } from "react";
+import ReactTable from "react-table";
 import "react-table/react-table.css";
 
 // import $ from 'jquery';
 //import BootstrapTable from 'reactjs-bootstrap-table';
 
 class Content extends Component {
-    
-    render() {
-      {console.log(this.props.data)}
-  
-       
-        return(<div>
-        <ReactTable
-          // data={uData}
-          columns={[
-            {
-              Header: "Company Info",
-              columns: [
+  render() {
+    console.log({ data2: this.props.data });
+
+    return (
+      <div>
+        {
+          this.props.data &&
+            <ReactTable
+              data={this.props.data}
+              columns={[
                 {
-                  Header: "Name",
-                  accessor: "company.name"
+                  Header: "Company Info",
+                  columns: [
+                    {
+                      Header: "Name",
+                      accessor: "company.name"
+                    },
+                    {
+                      Header: "Service",
+                      accessor: "company.bs"
+                    }
+                  ]
                 },
                 {
-                  Header: "Service",
-                  accessor: "company.bs"
+                  Header: "Address",
+                  columns: [
+                    {
+                      Header: "City",
+                      accessor: "address.city"
+                    },
+                    {
+                      Header: "Streat",
+                      accessor: "address.street"
+                    },
+                    {
+                      Header: "Suite",
+                      accessor: "address.suite"
+                    }
+                  ]
                 }
-              ]
-            },
-            {
-              Header: 'Address',
-              columns: [
-                {
-                  Header: "City",
-                  accessor: "address.city"
-                },{
-                  Header: "Streat",
-                  accessor: "address.street"
-                },{
-                  Header: "Suite",
-                  accessor: "address.suite"
-                }
-              ]
-            }
-          ]}
-          
-          className="-striped -highlight"
-        />
-       
-      </div>)
-
-    }
-
-
+              ]}
+              className="-striped -highlight"
+            />
+        }
+      </div>
+    );
+  }
 }
-
 
 export default Content;
